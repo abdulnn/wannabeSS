83a79266b756f7e2674b88e0654149b21ee08243
abdul <adbulrehmansyed241@gmail.com>
2021-12-13 17:41:59 +0530

    Error page and logo clicked redirection

---
+++ src/DefaultPage/Error/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import image from './../../assets/images/error.png'
+import './style.css'
+
+const Error = () => {
+    return (
+            <div className='errorimage'>
+                <h1>Error (you have found a page that does'nt exist) </h1> 
+                
+               <img src={image} alt='' />
+            </div>
+    )
+}
+
+export default Error
--- src/components/Header/subcomponent/Softsuavelogo/index.js
+++ src/components/Header/subcomponent/Softsuavelogo/index.js
@@ -1,11 +1,14 @@
 import "./style.css";
 import React from "react";
 import logo from "../../../../assets/images/Softsuavelogo.png";
+import {Link} from "react-router-dom";
 
 function Softsuavelogo() {
   return (
       <div className="header_softsuavelogo">
+        <Link to="/">
         <img src={logo} top="0" left="0" alt="" />
+        </Link>
       </div>
   );
 }
